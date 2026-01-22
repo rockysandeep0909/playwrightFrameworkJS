f641aef431e12e9a1e9943e055b9e221b4fc4b8a
rockysandeep0909 <rockysandeep0909@gmail.com>
2026-01-22 10:39:26 +0530

    add bdd feature and step file

--- features/login.feature
+++ features/login.feature
@@ -1,24 +1,38 @@
 Feature: Login
 
-Scenario: Successful login using valid credential
-    Given I am in login page of saucedemo
-    When I enter valid username  
-    When I enter valid Password
-    And I click on login button
-    Then user should be redirected to saucedemo page
+    Scenario: Successful login using valid credential
+        Given I am in login page of saucedemo
+        When I enter valid username
+        When I enter valid Password
+        And I click on login button
+        Then user should be redirected to saucedemo page
 
-Scenario: Invalid login
-    Given I am in login page of saucedemo
-    When I enter valid username  
-    When I enter invalid Password
-    And I click on login button
-    Then user should see an error message
+    Scenario: Invalid login
+        Given I am in login page of saucedemo
+        When I enter valid username
+        When I enter invalid Password
+        And I click on login button
+        Then user should see an error message
 
 
-Scenario : dashboard validation 
-    Given I am in login page of saucedemo
-    When I enter valid username  
-    When I enter valid Password
-    And I click on login button
-    Then user should be redirected to saucedemo page
+    Scenario: dashboard validation
+        Given I am in login page of saucedemo
+        When I enter valid username
+        When I enter valid Password
+        And I click on login button
+        Then user should be redirected to saucedemo page
+
+    @login_valid
+    Scenario Outline: Login with multiple credentials
+        Given I am in login page of saucedemo
+        When I enter username "<username>" and password "<password>"
+        And I click on login button
+        Then I should see a message "<message>"
+
+        Examples:
+            | username                | password     | message                  |
+            | standard_user           | secret_sauce | Products                 |
+            | locked_out_user         | secret_sauce | Epic sadface: Sorry, ... |
+            | problem_user            | secret_sauce | Products                 |
+            | visual_user | secret_sauce | Products                 |
 
--- features/step_definitions/loginsteps.js
+++ features/step_definitions/loginsteps.js
@@ -1,6 +1,8 @@
 const { Given, When, Then } = require('@cucumber/cucumber')
 const playwright = require('playwright')
 const { expect } = require('@playwright/test')
+const { LoginPage } = require('../../POM/LoginPage');
+
 
 Given('I am in login page of saucedemo', async function () {
   console.log("this is our first cucumber test running ..... lets get started")
@@ -9,10 +11,10 @@ Given('I am in login page of saucedemo', async function () {
   this.browser = await playwright.chromium.launch({ headless: false })
   this.context = await this.browser.newContext()
   this.page = await this.context.newPage()
-  
-  // Navigate to login page
-  await this.page.goto("https://www.saucedemo.com/")
-  console.log("Navigated to saucedemo login page")
+  // Use LoginPage POM to navigate to login page
+  this.loginPage = new LoginPage(this.page);
+  await this.loginPage.goToLoginPage();
+  console.log("Navigated to saucedemo login page via LoginPage POM")
 })
 
 When('I enter valid username', async function () {
@@ -20,9 +22,8 @@ When('I enter valid username', async function () {
   const validUsername = "standard_user"
   
   // Fill username field
-  
-  await this.page.fill('#user-name', validUsername)
-  console.log(`Entered username: ${validUsername}`)
+  await this.loginPage.enterUsername(validUsername)
+  console.log(`Entered username via LoginPage: ${validUsername}`)
 })
 
 When('I enter valid Password', async function () {
@@ -30,15 +31,15 @@ When('I enter valid Password', async function () {
   const validPassword = "secret_sauce"
   
   // Fill password field
-  await this.page.fill('#password', validPassword)
-  console.log(`Entered password: ${validPassword}`)
+  await this.loginPage.enterPassword(validPassword)
+  console.log(`Entered password via LoginPage: ${validPassword}`)
 })
 
 When('I click on login button', async function () {
   console.log("Clicking login button")
   
   // Click login button
-  await this.page.click('#login-button')
+  await this.loginPage.clickLogin()
   
 
 })
@@ -60,8 +61,8 @@ When('I enter invalid Password', async function () {
   const invalidPassword = "wrong_password"
   
   // Fill password field with invalid password
-  await this.page.fill('#password', invalidPassword)
-  console.log(`Entered invalid password: ${invalidPassword}`)
+  await this.loginPage.enterPassword(invalidPassword)
+  console.log(`Entered invalid password via LoginPage: ${invalidPassword}`)
 })
 
 Then('user should see an error message', async function () {
@@ -69,3 +70,19 @@ Then('user should see an error message', async function () {
            // Close browser after test
   await this.browser.close()
          });
+
+When('I enter username {string} and password {string}', async function (username, password) {
+  console.log(`Entering username: ${username} and password: ${password}`);
+  await this.loginPage.enterUsername(username);
+ await this.loginPage.enterPassword(password);
+});
+
+
+Then('I should see a message {string}', async function (message) {
+    console.log(`Verifying message: ${message}`);
+    //   if (message === 'Products') {
+//     await expect(this.page).toHaveTitle(/Products/);
+//   } else {
+//     await expect(this.page.locator('[data-test="error"]')).toContainText(message);
+//   }
+});
